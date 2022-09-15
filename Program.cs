d50536c092dbc830052b2653305ea19f12230408
NiNaa32 <112417273+NiNaa32@users.noreply.github.com>
2022-09-15 20:40:44 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+namespace ConsoleApp11
+{
+    class Program
+    {
+        
+        static void InmätningStudent(Student s)
+        {
+            Console.WriteLine("Ange studentinformation:");
+            Console.Write("Namn: ");
+            s.GetSetNamn = Console.ReadLine();
+            Console.Write("Antal poäng på provet: - heltal (0,60): ");
+            s.GetSetAntPoäng = short.Parse(Console.ReadLine()); //double.Parse - konvertera från string till dubbel
+            Console.Write("Medelpoäng i gymnasiet: ");
+            s.GetSetMedPoäng = float.Parse(Console.ReadLine()); //long.Parse - konvertera från string till long
+        }
+        static void UtskriftStudent(Student s)
+        {
+            Console.WriteLine("Namn: {0}", s.GetSetNamn);
+            Console.WriteLine("Antal poäng på provet: {0}", s.GetSetAntPoäng);
+            Console.WriteLine("Medelpoäng i gymnasiet: {0}", s.GetSetMedPoäng);
+            Console.WriteLine("Antal poäng i rankinglistan: {0}", s.poängRank());
+        }
+        //
+        //Kollar om det är över genomsnittet
+        static void överGenomsnittet(Student[] array, int n)
+        {
+            float snittet = 0.0f;//float literal
+            bool finns = false;
+            int i;
+            for (i = 0; i < n; i++)
+            {
+                snittet += array[i].poängRank();
+            }
+            if (n > 0) snittet /= n;
+            else return;
+            Console.WriteLine("Data om studerande vars antal poäng i rankningslistan är över genomsnittet: {0} och som inte var utmärkta är:",
+                snittet); //utmärkta >4,5
+            for (i = 0; i < n; i++)
+            {
+                if (array[i].poängRank() > snittet && array[i].GetSetMedPoäng < 4.5)
+                {
+                    finns = true;
+                    UtskriftStudent(array[i]);
+                }
+            }
+            if (finns == false)
+                
+        Console.WriteLine("Det finns inga!");
+        }
+        static float Skillnad(Student[] array, int n, Student s)
+        {
+            float max = array[0].poängRank();
+            for (int i = 1; i < n; i++)
+            {
+                if (array[i].poängRank() > max)
+                    max = array[i].poängRank();
+            }
+            return (max - s.poängRank());
+        }
+        static void Main(string[] args)
+        {
+            int n, i;
+            Console.Write("Ange antal studerande: ");
+            n = int.Parse(Console.ReadLine()); //int.Parse - konvertera från sträng till int
+            Student[] array = new Student[n];//en array av n pekare till data av typen Student - KONSTRUKTOR ANROPAS INTE
+            Student s = new Student("Johan Johannson", 55, 5);//ANROP AV initieringskonstruktor
+            for (i = 0; i < n; i++)
+            {
+                array[i] = new Student();//*** ANROP AV standardkonstruktor
+                InmätningStudent(array[i]);
+            }
+            Console.WriteLine();
+            överGenomsnittet(array, n);
+            Console.WriteLine();
+            Console.WriteLine("Skillnaden i antal poäng på rankinglistan över bästa elev i årets och förra årets prov är: {0}", 
+                Skillnad(array, n, s));
+        Console.ReadKey();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    public class Student
+    {
+        private string namn;
+        private float antPoäng;
+        private float mPoäng;
+        //standardkonstruktor
+        public Student()
+        {
+            namn = "";
+            antPoäng = 0;
+            mPoäng = 0.0f; //bokstavstyp float slutar med f!!!
+            Console.WriteLine("\n***Standard konstruktoranrop!");
+        }
+        //initieringskonstruktor
+        public Student(string n, float ap, float mp)
+        {
+            namn = n; //kopiera sträng n till sträng namn
+            antPoäng = ap;
+            mPoäng = mp;
+            Console.WriteLine("\n***Anrop av initieringskonstruktör!");
+        }
+        //funktioner för att komma åt medlemmars privata data (GET SET)
+        public string GetSetNamn
+        {
+            get { return namn; }
+            set { namn = value; }
+        }
+        public float GetSetAntPoäng
+        {
+            get { return antPoäng; }
+            set { antPoäng = value; }
+        }
+        public float GetSetMedPoäng
+        {
+            get { return mPoäng; }
+            set { mPoäng = value; }
+        }
+        
+        public float poängRank() //att bilda den slutliga rankning poäng 
+        {
+            return (antPoäng + mPoäng * 8);
+        }
+    }
+}
